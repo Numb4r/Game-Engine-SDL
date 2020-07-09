47f95cdb730667ca77b6141519442f631eda9ca3
Numb4r <yuri.d.d.faria@gmail.com>
2020-07-09 15:51:42 -0300

    created a game loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "./Game.hpp"
+#include "./Constants.h"
 
-int main()
+int main(int argc, char *args[])
 {
+    //TDOO:
+    Game *game = new Game();
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    while (game->IsRunning())
+    {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+    game->Destroy();
     return 0;
 }
