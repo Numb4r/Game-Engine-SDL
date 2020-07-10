4f7cabb910f9f8ccb49530b13be1576038029202
Numb4r <yuri.d.d.faria@gmail.com>
2020-07-10 13:38:18 -0300

    created entitymanager class

---
+++ src/EntityManager.hpp
@@ -0,0 +1,22 @@
+#ifndef ENTITYMANAGER_HPP
+#define ENTITYMANAGER_HPP
+#include "./Entity.hpp"
+#include "./Component.hpp"
+#include <vector>
+
+class EntityManager
+{
+private:
+    std::vector<Entity *> entities;
+
+public:
+    void ClearData();
+    void Update(float deltaTime);
+    void Render();
+    bool HasNoEntities();
+    Entity &AddEntity(std::string entityName);
+    std::vector<Entity *> GetEntities() const;
+    unsigned int GetEntityCount();
+};
+
+#endif
