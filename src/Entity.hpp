636f713139c934f301950e1b79ddb873fbd007fb
Numb4r <yuri.d.d.faria@gmail.com>
2020-07-14 18:18:25 -0300

    created listcomponent func / template to get/has components

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "./EntityManager.hpp"
 #include "./Component.hpp"
-
+#include <map>
 class Component;
 class EntityManager;
 class Entity
@@ -13,6 +13,7 @@ private:
     EntityManager &manager;
     bool isActive;
     std::vector<Component *> components;
+    std::map<const std::type_info *, Component *> componentTypeMap;
 
 public:
     std::string name;
@@ -22,15 +23,26 @@ public:
     void Render();
     void Destroy();
     bool IsActive() const;
-
+    void ListComponents() const;
     template <typename T, typename... TArgs>
     T &AddComponent(TArgs &&... args)
     {
         T *newComponent(new T(std::forward<TArgs>(args)...));
         newComponent->owner = this;
         components.emplace_back(newComponent);
+        componentTypeMap[&typeid(*newComponent)] = newComponent;
         newComponent->Initialize();
         return *newComponent;
+    }
+    template <typename T>
+    T *GetComponent()
+    {
+        return static_cast<T *>(componentTypeMap[&typeid(T)]);
+    }
+    template <typename T>
+    bool HasComponent() const
+    {
+        return !componentTypeMap.empty();
     }
 };
 #endif
