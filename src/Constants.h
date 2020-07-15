418c9515e877f607fb077f8d85f304bf79a43aec
Numb4r <yuri.d.d.faria@gmail.com>
2020-07-15 16:35:11 -0300

    created enum for sprite layer

--- src/Constants.h
+++ src/Constants.h
@@ -5,4 +5,15 @@ const unsigned int WINDOW_HEIGHT = 600;
 const unsigned int FPS = 60;
 const unsigned int FRAME_TARGET_TIME = 1000 / FPS;
 
+enum LayerType
+{
+    TILEMAP_LAYER = 0,
+    VEGETATION_LAYER = 1,
+    ENEMY_LAYER = 2,
+    PLAYER_LAYER = 3,
+    PROJECTILES_LAYER = 4,
+    UI_LAYER = 5,
+};
+const unsigned int NUM_LAYER = 6;
+
 #endif
