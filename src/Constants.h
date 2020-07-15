9ffc0eba0cd60d30953066836b1678f55f4f6dbd
Numb4r <yuri.d.d.faria@gmail.com>
2020-07-15 19:16:41 -0300

    declaring enum to deal with collisitons

--- src/Constants.h
+++ src/Constants.h
@@ -5,6 +5,16 @@ const unsigned int WINDOW_HEIGHT = 600;
 const unsigned int FPS = 60;
 const unsigned int FRAME_TARGET_TIME = 1000 / FPS;
 
+enum CollisionType
+{
+    NO_COLLISION,
+    PLAYER_ENEMY_COLLISION,
+    PLAYER_PROJECTILE_COLLISION,
+    ENEMY_PROJECTILE_COLLISION,
+    PLAYER_VEGETATION_COLLISION,
+    PLAYER_LEVEL_COMPLETE_COLLISION
+
+};
 enum LayerType
 {
     TILEMAP_LAYER = 0,
@@ -12,8 +22,9 @@ enum LayerType
     ENEMY_LAYER = 2,
     PLAYER_LAYER = 3,
     PROJECTILES_LAYER = 4,
-    UI_LAYER = 5,
+    OBSTACLE_LAYER = 5,
+    UI_LAYER = 6,
 };
-const unsigned int NUM_LAYER = 6;
+const unsigned int NUM_LAYER = 7;
 
 #endif
